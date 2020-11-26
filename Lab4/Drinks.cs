8d9a200d43782cdd6ef197a0c752bdae9180e551
Виктория Иванова <viktoriiaivanova101@gmail.com>
2020-11-26 19:27:48 +0800

    initial commit

---
+++ Lab4/Drinks.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Drinks  //Создание класса напитков
+    {
+        public int Volume = 0;  //общее свойство - Объём
+        public static Random rnd = new Random();
+        public virtual String GetInfo()
+        {
+           var str = String.Format("\nОбъём: {0} мл ", this.Volume);
+            return str;
+        }
+
+    }
+    public enum Fruit {Яблоко, Апельсин, Персик};  //Перечисление фруктов для сока
+    public class Juice : Drinks  //Класс для сока
+    {
+        public Fruit fruittype = Fruit.Яблоко;  //Используемый фрукт
+        public bool Pulp = false;  //наличие мякоти
+        public override String GetInfo()  //Составление и вывод информации об объекте класса
+        {
+            string pulp;
+            if (this.Pulp)
+            {
+                pulp = "есть";
+            }
+            else
+            {
+                pulp = "отсутствует";
+            }
+            var str = "Сок";
+            str += base.GetInfo();
+            str += String.Format("\nИспользуемый фрукт: {0}", this.fruittype);
+            str += String.Format("\nМякоть: {0}", pulp);
+            return str;
+        }
+        
+        public static Juice Generate()  //Создание свойств класса
+        {
+            return new Juice
+            {
+                Volume = rnd.Next() % 100, // объём от 0 до 100
+                fruittype = (Fruit)rnd.Next(3), // используемый фрукт
+                Pulp = rnd.Next() % 2 == 0 // наличие мякоти true или false
+            };
+        }
+    }
+    public enum SodaType {Cola, Pepsi, Fanta};  //Перечисление типов газировки
+    public class Soda : Drinks  //Класс для газировки
+    {
+        public SodaType sodatype = SodaType.Cola;  //Тип газировки
+        public int Bubbles = 0;  //Количество пузыриков
+        public override String GetInfo()  //Составление и вывод информации об объекте класса
+        {
+            var str = "Газировка";
+            str += base.GetInfo();
+            str += String.Format("\nВид: {0}", this.sodatype);
+            str += String.Format("\nКоличество пузыриков: {0}", this.Bubbles);
+            return str;
+        }
+        public static Soda Generate()  //Создание свойств класса
+        {
+            return new Soda
+            {
+                Volume = rnd.Next() % 100, // объём от 0 до 100
+                sodatype = (SodaType)rnd.Next(3), // Вид газировки
+                Bubbles = rnd.Next() % 500  // количество пузыриков от 0 до 500
+            };
+        }
+    }
+    public enum AlcoholType {Ром, Коньяк, Виски}  //Перечисление типов алкоголя
+    public class Alcohol : Drinks  //Класс для алкоголя
+    {
+        public int Strength = 0;  //Крепость 
+        public AlcoholType alcoholtype = AlcoholType.Виски;  //Тип алкоголя
+        public override String GetInfo()  //Составление и вывод информации об объекте класса
+        {
+            var str = "Алкоголь";
+            str += base.GetInfo();
+            str += String.Format("\nКрепость: {0} %", this.Strength);
+            str += String.Format("\nТип: {0}", this.alcoholtype);
+            return str;
+        }
+        public static Alcohol Generate()  //Создание свойств класса
+        {
+            return new Alcohol
+            {
+                Volume = rnd.Next() % 100, // объём от 0 до 100
+                Strength = rnd.Next() % 50, // крепость алкоголя от 0 до 50
+                alcoholtype = (AlcoholType)rnd.Next(3) // тип алкоголя
+            };
+        }
+    }
+    
+}
---
+++ Lab4/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        List<Drinks> drinksList = new List<Drinks>();  //список напитков
+        public Form1()
+        {
+            InitializeComponent();
+            ShowInfo();
+        }
+
+        private void btnRefill_Click(object sender, EventArgs e)  //Обработчик кнопки "Перезаполнить"
+        {
+            this.drinksList.Clear();
+            var rnd = new Random();
+            for (var i = 0; i < 15; ++i)
+            {
+                switch (rnd.Next() % 3) // генерирую случайное число от 0 до 2 для выбора напитка
+                {
+                    case 0:
+                        this.drinksList.Add(Juice.Generate());
+                        break;
+                    case 1:
+                        this.drinksList.Add(Soda.Generate());
+                        break;
+                    case 2:
+                        this.drinksList.Add(Alcohol.Generate());
+                        break;
+                }
+            }
+            ShowInfo();
+        }
+        private void ShowInfo() // Функция, выводящая информацию о количестве напитков на форму
+        {
+            listOfDrinks.Clear();  //Список очереди напитков
+            // счетчики под каждый тип
+            int juiceCount = 0;
+            int sodaCount = 0;
+            int alcoholCount = 0;
+
+            foreach (var drink in this.drinksList)
+            {
+                //Проверка на то, какой объект типа Drinks
+                if (drink is Juice)
+                {
+                    juiceCount += 1;
+                    listOfDrinks.Text += "Сок\n";  //Добавление "Сок" в список очереди
+                }
+                else if (drink is Soda)
+                {
+                    sodaCount += 1;
+                    listOfDrinks.Text += "Газировка\n";  //Добавление "Газировка" в список очереди
+                }
+                else if (drink is Alcohol)
+                {
+                    alcoholCount += 1;
+                    listOfDrinks.Text += "Алкоголь\n";  //Добавление "Алкоголь" в список очереди
+                }
+            }
+
+            // Вывод количества объектов на форму
+            txtInfo.Text = "               Сок              Газировка              Алкоголь";
+            txtInfo.Text += "\n";
+            txtInfo.Text += String.Format("                 {0}                         {1}                            {2}", juiceCount, sodaCount, alcoholCount);
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)  //Обработчик кнопки "Взять"
+        {
+            if (this.drinksList.Count == 0)
+            {
+                txtOut.Text = "Пусто ... больше нет напитков?";
+                pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\empty.jpg");
+                return;
+            }
+            var drink = this.drinksList[0];
+            this.drinksList.RemoveAt(0);
+
+            txtOut.Text = drink.GetInfo();
+            //Добавление картинок
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            if (drink is Juice)
+            { 
+                if (txtOut.Text.Contains("Апельсин"))
+                {
+                    pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Апельсин.jpg");
+                }
+                else if (txtOut.Text.Contains("Персик"))
+                {
+                    pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Персик.jpg");
+                }
+                else
+                {
+                    pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Яблоко.jpg");
+                }
+               
+            }
+            else if (drink is Soda)
+            {
+                if (txtOut.Text.Contains("Cola"))
+                {
+                    pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Cola.jpg");
+                }
+                else if (txtOut.Text.Contains("Pepsi"))
+                {
+                    pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Pepsi.jpg");
+                }
+                else
+                {
+                    pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Fanta.jpg");
+                }
+            }
+            else
+            {
+                if (txtOut.Text.Contains("Ром"))
+                {
+                    pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Ром.jpg");
+                }
+                else if (txtOut.Text.Contains("Коньяк"))
+                {
+                    pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Коньяк.jpg");
+                }
+                else
+                {
+                    pictureBox1.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Виски.jpg");
+                }
+            }
+            ShowInfo();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
